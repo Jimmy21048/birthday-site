5e80e4e50961f2aee39a3225da14501ef796cc2b
Jimmy Rubia <rubiajimmy3@gmail.com>
2024-06-14 21:09:25 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Signup from './Signup';
+
+export default function App() {
+    return (
+        <div className="app">
+            <Signup />
+        </div>
+    )
+}
